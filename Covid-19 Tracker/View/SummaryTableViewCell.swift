1938e46152e9d38808e2aeee4e54bd52d7a18798
Jimit Raval <34153272+jimmi1998@users.noreply.github.com>
2020-05-20 21:19:54 +0530

    Add files via upload

---
+++ Covid-19 Tracker/View/SummaryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryTableViewCell.swift
+//  Covid-19 Tracker
+//
+//  Created by Jimit Raval on 27/04/20.
+//  Copyright © 2020 Mango. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SummaryTableViewCell : UITableViewCell {
+
+    @IBOutlet weak var ImageCountry: UIImageView!
+    @IBOutlet weak var TotalConfirmed: UILabel!
+    @IBOutlet weak var TotalRecovered: UILabel!
+
+}
